aa61d73a814baa36990a4b76618f537e30e761ba
Michael Tinsley <michaeltinsley@outlook.com>
2024-12-04 16:14:25 +0000

    feat: day 04 - part 01

---
+++ day_04/test_main.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+from day_04.main import solve_xmas_word_search
+
+
+def test_part_01():
+    path = Path("day_04/test_data.txt")
+    expected_result = 2
+    result = solve_xmas_word_search(path)
+    assert result == expected_result, f"Expected {expected_result} but got {result}"
